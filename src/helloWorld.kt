0aeede30ecc995ad6352e431cb1c6ee83006592d
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2024-02-27 23:42:38 +0530

    Working with Data Types and variables.

--- src/helloWorld.kt
+++ src/helloWorld.kt
@@ -1,3 +1,16 @@
-fun main(){
-    println("Hello, World!");
+fun main() {
+    println("I'm")
+    println("learning")
+    println("Kotlin")
+
+    //another way of defining a variable with explicit type
+    //val count: Int = 2
+    //var count: Int = 4
+    val count = 2
+    //count = 5
+    var num = 15
+
+    num = 10
+    println("You have $count unread messages.")
+    println(num)
 }
