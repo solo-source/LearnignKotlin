bea8b65cd20aac88a27824dcb8ae1c25707b6819
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2024-03-05 00:34:14 +0530

    Performed basic math operations of variables and used var and val

---
+++ src/basicMathOperation.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val unreadEmailCount = 5
+    val readEmailCount = 100
+    println("You have ${unreadEmailCount + readEmailCount} total messages in your inbox.")
+    val photoAvailable = 100
+    val deletedPhoto = 10
+    println("$photoAvailable photos.")
+    println("$deletedPhoto photos deleted.")
+    println("${photoAvailable - deletedPhoto} photos left.")
+
+    var cartTotal = 20
+    cartTotal = 30
+    println("Total: $cartTotal")
+}
